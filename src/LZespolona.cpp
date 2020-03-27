c528ffac2b611df41159d8b5514939c875552353
siarka <58476699+siarka@users.noreply.github.com>
2020-03-27 12:56:44 +0100

    problem

---
+++ src/LZespolona.cpp
@@ -0,0 +1,122 @@
+#include "LZespolona.hh"
+#include <iostream>
+using namespace std;
+
+  
+
+
+
+/*!
+ * Realizuje dodanie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik dodawania,
+ *    Skl2 - drugi skladnik dodawania.
+ * Zwraca:
+ *    Sume dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Skl1.re + Skl2.re;
+  Wynik.im = Skl1.im + Skl2.im;
+  return Wynik;
+}
+/*!
+ * Realizuje dodanie odejmowanie liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - odjemna,
+ *    Skl2 - odjemnik.
+ * Zwraca:
+ *    roznice dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator - (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona Wynik;
+
+
+  Wynik.re = Skl1.re - Skl2.re;
+  Wynik.im = Skl1.im - Skl2.im;
+  return Wynik;
+
+}
+/*!
+ * Realizuje dodanie mnozenia liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik mnozenia,
+ *    Skl2 - drugi skladnik mnoenia.
+ * Zwraca:
+ *    Iloraz dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator * (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Skl1.re * Skl2.re - Skl1.im * Skl2.im;
+  Wynik.im = Skl1.re * Skl2.im + Skl2.re * Skl1.im;
+  return Wynik;
+}
+/*!
+ * Realizuje dzielenie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - licznik,
+ *    Skl2 - mianownik.
+ *    -nie sprawdzam czy jest zerowy poniwaz sam go ustalam
+ * Zwraca:
+ *    iloraz  dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator / (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = (Skl1.re * Skl2.re - Skl1.im * Skl2.im) / (Skl2.re * Skl2.re + Skl2.im * Skl2.im);
+  Wynik.im = (Skl1.im * Skl2.re - Skl1.re * Skl2.im) / (Skl2.re * Skl2.re + Skl2.im * Skl2.im);
+  return Wynik;
+}
+
+
+
+istream& operator>>(istream& StrmWe, LZespolona& Lz){
+  int l = 1;
+  char niep;
+  
+  StrmWe.clear();
+  cout<<"Twoja odpowiedz: ";
+
+  StrmWe >> niep;
+  if(niep != '('){StrmWe.setstate(ios::failbit);}
+  StrmWe >> Lz.re >> Lz.im;
+  StrmWe >> niep;
+  if(niep != 'i'){StrmWe.setstate(ios::failbit);}
+  StrmWe >> niep;
+  if(niep != ')'){StrmWe.setstate(ios::failbit);}
+  StrmWe.ignore();
+
+
+
+  while(StrmWe.fail()){
+  cerr<<"Blad formatu liczby zespolonej sprobuj ponownie"<<endl;
+    if(l == 3){
+      return StrmWe;
+    }
+
+    StrmWe.clear();
+    StrmWe.ignore(1000, '\n');
+
+    
+
+    StrmWe >> niep;
+    if(niep != '('){StrmWe.setstate(ios::failbit);}
+    StrmWe >> Lz.re >> Lz.im;
+    StrmWe >> niep;
+    if(niep != 'i'){StrmWe.setstate(ios::failbit);}
+    StrmWe >> niep;
+    if(niep != ')'){StrmWe.setstate(ios::failbit);}
+    StrmWe.ignore();
+
+    cout<<endl;
+
+    l++;
+  }
+  return StrmWe;
+}
